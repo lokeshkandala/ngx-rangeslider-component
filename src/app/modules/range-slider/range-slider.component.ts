601248db9f646c950674e2ae00e2e9d3de92c910
lokeshkandala <nagalokesh09@gmail.com>
2022-05-21 20:23:19 +0530

    Fix: Fixed css issues for demopage

--- src/app/modules/range-slider/range-slider.component.ts
+++ src/app/modules/range-slider/range-slider.component.ts
@@ -308,7 +308,7 @@ getlength(num:any): number {
     let pixel = this.valToPixelFactor * (value - this.min);
     return pixel;
   }
-  @HostListener('mousemove', ['$event'])
+  @HostListener('window:mousemove', ['$event'])
   mouseMove(event: any) {
     if(this.minSelected || this.maxSelected){
     if (this.minSelected) {
