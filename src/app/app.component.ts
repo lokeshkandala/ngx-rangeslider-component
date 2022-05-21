998855027edbd715efdbe5b0e70419097012c50d
lokeshkandala <nagalokesh09@gmail.com>
2022-05-21 18:14:00 +0530

    Fix: Updated packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'range-slider';
+  title = 'app';
+  min=5;
+  max=59;
+  range=[1400,1600];
+  min1=-10;
+  max1=50;
+  step=5;
+  twoWayRange=[10,30];
+  customStyleRange=[5,38];
+  stepRange=[25,40];
+  dumyForm = {
+    range: [9,38]
+};
+  changed()
+  {
+    this.twoWayRange=[...this.twoWayRange];
+  }
+  onSubmit() {  }
+  rangeChanged(event:any)
+  {
+   console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RangeSliderModule } from './modules/range-slider/range-slider.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RangeSliderModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
